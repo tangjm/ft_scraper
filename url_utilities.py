33e52042679b3ad41746edf4bb4da97d3e1b0688
tangjm <jaredtang@hotmail.co.uk>
2022-09-15 22:52:20 +0100

    Add error handling for url related operations.

--- url_utilities.py
+++ url_utilities.py
@@ -1,8 +1,12 @@
+from os import strerror
 from urllib import request
+from urllib.error import URLError
 
 def fetch_content(url):
   try: 
     response = request.urlopen(url)
     return response.read()
-  except:
-    print("Request failed")
+  except URLError:
+    print("Error opening url: ", URLError)
+  except IOError as e:
+    print("I/O Error: ", strerror(e.errno))
